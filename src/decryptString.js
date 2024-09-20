8fde2af9bb4607cbeaa3959be6ce16714c0bda13
Chad <chadillac@Chads-MacBook-Air.local>
2024-09-20 09:56:48 -0600

    Add decryptString.js script

---
+++ src/decryptString.js
@@ -0,0 +1,7 @@
+import { argv } from "process";
+import Encryption from './libs/Encryption'
+
+const enc = Encryption('risk3sixty') // update to make dynamic
+// const plainText = enc.decrypt(ciphertext); 
+
+console.log("hello");
